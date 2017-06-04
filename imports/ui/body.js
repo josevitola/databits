0522abc23dc37efc713d772dcbf64368bfcdfcd3
Jose Nieto <jdnv.123@gmail.com>
2017-06-04 11:12:06 -0500

    Install and test C3 as Meteor package

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -32,6 +32,28 @@ Meteor.startup(function() {
 Template.body.onRendered(function bodyOnRendered() {
   this.$('.ui.accordion').accordion();
   initLoginMessage(this);
+  Session.set('x', ['x', 30, 50, 75, 100, 120]);
+  Session.set('data1', ['data1', 30, 200, 100, 400, 150]);
+  Session.set('data2', ['data2', 20, 180, 240, 100, 190]);
+  var chart = c3.generate({
+    bindto: this.find('#chart'),
+      data: {
+        xs: {
+          'data1': 'x',
+          'data2': 'x'
+        },
+        columns: [['x'],['data1'],['data2']]
+      }
+  });
+
+  this.autorun(function (tracker) {
+    chart.load({columns: [
+      Session.get('x'),
+      Session.get('data1'),
+      Session.get('data2'),
+      []
+    ]});
+  });
 });
 
 Template.body.events({
