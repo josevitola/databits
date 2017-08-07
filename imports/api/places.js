8211d0e9c4efb8ef67632d007dc9993a0f27a309
Jose Nieto <jdnv.123@gmail.com>
2017-08-07 18:39:53 -0500

    Open infowindow with card click (TEMPORARY)

--- imports/api/places.js
+++ imports/api/places.js
@@ -3,3 +3,13 @@ import { Mongo } from 'meteor/mongo';
 Place = new Mongo.Collection('places');
 
 export const Places = Place;
+
+export const getPinImgName = function(type) {
+  if (type == "restaurant") {
+    return "rest";
+  } else if (type == "museum") {
+    return "muse";
+  } else if (type == "theatre") {
+    return "teat";
+  }
+}
